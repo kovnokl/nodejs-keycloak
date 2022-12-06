b40154772561654be68d0913edc9e433fc32c4a6
Kevin Nickel <kevinnickel2003@gmail.com>
2022-12-06 09:08:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { auth } = require('express-openid-connect');
+
+app.use(
+    auth({
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET
+    })
+);
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
