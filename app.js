8ea955af5a693590f2647f6d5d614a627baf9f63
Kevin Nickel <kevinnickel2003@gmail.com>
2022-12-06 09:33:57 +0100

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(
         issuerBaseURL: process.env.ISSUER_BASE_URL,
         baseURL: process.env.BASE_URL,
         clientID: process.env.CLIENT_ID,
-        secret: process.env.SECRET
+        secret: process.env.SECRET,
     })
 );
 
