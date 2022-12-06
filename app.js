5a43c8c2e4ba59aba558ccecb80986615cbba5c1
Kevin Nickel <kevinnickel2003@gmail.com>
2022-12-06 09:38:33 +0100

    root route

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const { auth } = require('express-openid-connect');
 
 app.use(
     auth({
+        authRequired: false,
+        auth0Logout: true,
         issuerBaseURL: process.env.ISSUER_BASE_URL,
         baseURL: process.env.BASE_URL,
         clientID: process.env.CLIENT_ID,
         secret: process.env.SECRET,
     })
 );
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+})
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
